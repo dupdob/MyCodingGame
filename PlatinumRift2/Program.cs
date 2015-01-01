20d1f56970f69f68d5895334955eb44e29bfa9ef
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2015-01-01 23:02:07 +0100

    Add version in error output Give boost for enemy base

--- PlatinumRift2/Program.cs
+++ PlatinumRift2/Program.cs
@@ -3,12 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System;
-using System.Linq;
 using System.IO;
-using System.Text;
 using System.Collections;
-using System.Collections.Generic;
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -16,13 +12,40 @@ using System.Collections.Generic;
  **/
 class Zone
 {
+	private int owner;
+	static public int MyID;
+
 	public List<Zone> Links = new List<Zone>();
+	public int ID;
+	public bool EnemyBase;
+
 	public Dictionary<int, int> Pods = new Dictionary<int, int>(); 
 	public int Platinum;
-	public int Owner;
-	public int ID;
+	public int Owner
+	{
+		get { return this.owner;}
+		set {
+			if (this.owner != value) {
+				if (value != MyID)
+					VisitCount = 0;
+			}
+		}
+	}
 
+	public int VisitCount;
 
+	public int ComputeScore(int podsCount)
+	{
+		if (this.EnemyBase)
+			return 1000;
+		if (Owner < 0) { // no owner
+			return 10*(1+Platinum);
+		}
+		if (Owner != MyID) {
+			return 50*(1+Platinum);
+		}
+		return (1+Platinum)-VisitCount;
+	}
 }
 
 class Pod
@@ -34,9 +57,10 @@ class Player
 {
 	static void Main(string[] args)
 	{
+		Console.Error.WriteLine ("dupdob: Algo V2");
 		string[] inputs = Console.ReadLine().Split(' ');
-		int playerCount = int.Parse(inputs[0]); // the amount of players (2 to 4)
-		int myId = int.Parse(inputs[1]); // my player ID (0, 1, 2 or 3)
+//		int playerCount = int.Parse(inputs[0]); // the amount of players (2 to 4)
+		Zone.MyID = int.Parse(inputs[1]); // my player ID (0, 1, 2 or 3)
 		int zoneCount = int.Parse(inputs[2]); // the amount of zones on the map
 		int linkCount = int.Parse(inputs[3]); // the amount of links between all zones
 		var rnd = new Random (0);
@@ -55,11 +79,12 @@ class Player
 			var z1 = int.Parse (inputs [0]);
 			var z2 = int.Parse (inputs [1]);
 			zones[z1].Links.Add(zones[z2]);
-			zones [z2].Links.Add (zones [z1]);
+			zones[z2].Links.Add (zones [z1]);
 		}
 
+		bool firstRound = true;
 		// game loop
-		while (true)
+		for(;;)
 		{
 			var myPods = new List<Pod>(10);
 
@@ -73,12 +98,13 @@ class Player
 				{
 					var pods = int.Parse(inputs[2 + j]);
 					zone.Pods[j] = pods;
-					if (pods > 0 && j == myId)
-					{
-						var pod = new Pod();
+					if (j == Zone.MyID) {
+						var pod = new Pod ();
 						pod.CurrentZone = i;
 						for (var k = 0; k < pods; k++)
-							myPods.Add(pod);
+							myPods.Add (pod);
+					} else if (firstRound && pods > 0) {
+						zone.EnemyBase = true;
 					}
 				}
 			}
@@ -90,21 +116,25 @@ class Player
 				var zone = zones [pod.CurrentZone];
 				var nbNeighbour = zone.Links.Count;
 				var offset = rnd.Next (nbNeighbour - 1);
-				var target = zone.Links[ offset ].ID;
+				var scoring = new SortedList<int, int> (6);
 				// pod logic: scan all neigbour, choose one I do not own
 				for (var j = 0; j < nbNeighbour; j++) {
 					var neighbour = zone.Links [(j + offset) % nbNeighbour];
-					if (neighbour.Owner < 0)
-						target = neighbour.ID;
-					else if (neighbour.Owner != myId) {
-						target = neighbour.ID;
-						break;
-					}
+					scoring [neighbour.ComputeScore (1)] = neighbour.ID;
 				}
-				Console.Write ("1 {0} {1} ", pod.CurrentZone, target);
+
+				Console.Error.Write ("Pod #{0} from {1}", i, pod.CurrentZone);
+				foreach (var dump in scoring) {
+					Console.Error.Write ("{0} {1};", dump.Value, dump.Key);
+				}
+				Console.Error.WriteLine ();
+				var nextZone = scoring.Last ().Value;
+				zones [nextZone].VisitCount++;
+				Console.Write ("1 {0} {1} ", pod.CurrentZone, nextZone);
 			}
 			Console.WriteLine(""); // first line for movement commands, second line for POD purchase (see the protocol in the statement for details)
 			Console.WriteLine("WAIT");
+			firstRound = false;
 		}
 	}
-}
+} 
