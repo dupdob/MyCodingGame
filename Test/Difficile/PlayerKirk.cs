2b8cc11e3419ca4e9c83e95885279a96712581a1
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2015-03-21 23:03:59 +0100

    [Kirk - Hard] : 80% coverage. Scanning deficient

--- Test/Difficile/PlayerKirk.cs
+++ Test/Difficile/PlayerKirk.cs
@@ -14,6 +14,17 @@ class PlayerKirk
 
 	enum Direction {Right, Down, Left, Up, None};
 
+	static void Log(string fmt, params object[] obs)
+	{
+		Console.Error.Write (fmt, obs);
+	}
+
+	static void LogLine(string fmt, params object[] obs)
+	{
+		Console.Error.WriteLine (fmt, obs);
+	}
+
+
 	class Coordinates
 	{
 		public int R;
@@ -46,9 +57,20 @@ class PlayerKirk
 			return (other != null && other.R == this.R && other.C == this.C);
 		}
 
+		public bool Equals(Coordinates other)
+		{
+			return (other != null && other.R == this.R && other.C == this.C);
+		}
+
+
 		public override int GetHashCode()
 		{
 			return R * 10000 + C;
+		}
+
+		public override string ToString ()
+		{
+			return string.Format ("{0}:{1}", C, R);
 		}
 	}
 
@@ -59,6 +81,8 @@ class PlayerKirk
 		public Coordinates Teleport;
 
 		public Coordinates Control;
+
+		public int Countdown;
 
 		public Plan(int R, int C)
 		{
@@ -80,52 +104,93 @@ class PlayerKirk
 			return room [coord.R, coord.C] == '?';
 		}
 
-		private Direction PathFromTo(Coordinates start, Coordinates to, List<Coordinates> path, Direction lastDir, ref int minLength)
+		public bool CanGoThrough(Coordinates coord)
 		{
-			if (path.Where((x) => x.Equals(start)).Any()|| path.Count == minLength) {
-				return Direction.None;
-			}
-
-			if (start.Equals(to)) {
-				Console.Error.WriteLine ("Found path to dest");
-				minLength = path.Count;
-				return Direction.Up;
-			}
-
-			Console.Error.Write("{0}.{1}, ", start.R, start.C);
-			var updatedPath = new List<Coordinates> (path);
-			updatedPath.Add (start);
-
-			Direction result = Direction.None;
-
-			var directions = new ArrayList (typeof(Direction).GetEnumValues ());
-			directions.Remove (lastDir);
-			directions.Insert (0, lastDir);
-			directions.Remove (Direction.None);
-			foreach (Direction value in directions) {
-				var next = start.Move (value);
-				if (next == null || IsAWall (next) || IsUnknonw(next)) {
-					continue;
-				}
-				int length = minLength;
-
-				var tempresult = PathFromTo (next, to, updatedPath, value, ref length);
-				if (tempresult != Direction.None && length<minLength) {
-					Console.Error.WriteLine ("Found path going {0} (l:{1})", value, length);
-					minLength = length;
-					result = value;
-				}
-			}
-			return result;
+			return !IsAWall (coord) && !IsUnknonw (coord);
 		}
-
-		public Direction PathFromTo(Coordinates from, Coordinates to)
+			
+		public Direction PathFromTo(Coordinates from, Coordinates to, out int len)
 		{
-			Console.Error.WriteLine ("Start Scan path to {0}, {1}", to.R, to.C);
-			var path = new List<Coordinates> ();
-			int length = int.MaxValue;
-			var result = PathFromTo (from, to, path, Direction.None, ref length);
-			Console.Error.WriteLine("Path length is {0}.", length);
+			LogLine ("Start Scan path from {0} to {1}.", from, to);
+
+			var distances = new Dictionary<Coordinates, int> (room.Length);
+			var nodesToScan = new List<Coordinates> (room.Length);
+			var previous = new Dictionary<Coordinates, Coordinates> (room.Length);
+
+			for(int i=0; i<room.GetLength(0); i++) {
+				for (int j = 0; j < room.GetLength (1); j++) {
+					var cell = new Coordinates (i, j);
+					if (CanGoThrough(cell))
+						nodesToScan.Add (cell);
+				}
+			}
+			distances [from] = 0;
+
+			len = int.MaxValue;
+
+			while (nodesToScan.Count > 0) {
+				// find closest node to source
+				Coordinates closest = null;
+				var minLength = int.MaxValue;
+				foreach (var node in nodesToScan) {
+					int dist;
+					if (distances.TryGetValue (node, out dist)) {
+						if (dist < minLength) {
+							minLength = dist;
+							closest = node;
+						}
+					}
+				}
+				if (closest == null) {
+					PlayerKirk.LogLine ("No path to target");
+					return Direction.None;
+				}
+
+				PlayerKirk.LogLine("Closest Node : {0}", closest);
+				nodesToScan.Remove (closest);
+				if (closest.Equals (to)) {
+					PlayerKirk.LogLine ("Found path to dest");
+					break;
+				}
+				// scan neighbours
+				foreach (Direction value in typeof(Direction).GetEnumValues()) {
+					var neighbor = closest.Move (value);
+					if (neighbor != null && CanGoThrough (neighbor)) {
+						var newdist = minLength +1;
+						int oldDist;
+						if (!distances.TryGetValue(neighbor, out oldDist) || oldDist > newdist){
+							distances[neighbor] = newdist;
+							previous [neighbor] = closest;
+						}
+					}
+				}
+			}
+			// find the path;
+			var scan = to;
+			Log ("Unwinding path from {0}, ", to);
+			var result = Direction.None;
+			while(true) {
+				if (previous.ContainsKey (scan)) {
+					var prev = previous [scan];
+					if (prev.Equals(from)) {
+						break;
+					}
+					scan = prev;
+				} else {
+					break;
+				}
+			}
+
+			len = distances[to];
+
+			if (scan != null) {
+				LogLine ("Moving to {0}", scan);
+				if (from.C == scan.C) {
+					result = from.R > scan.R ? Direction.Up : Direction.Down;
+				} else {
+					result = from.C > scan.C ? Direction.Left : Direction.Right;
+				}
+			}
 			return result;
 		}
 
@@ -145,7 +210,7 @@ class PlayerKirk
 			}
 
 			if (IsUnknonw(start)) {
-				Console.Error.WriteLine ("Found unknown cell at {0}:{1}", start.R, start.C);
+				Console.Error.WriteLine ("Found unknown cell at {0}", start.R, start.C);
 				minLength = path.Count;
 				return Direction.Up;
 			}
@@ -217,6 +282,7 @@ class PlayerKirk
 
 		bool goToControl = true;
 		var plan = new Plan (R, C);
+		plan.Countdown = A;
 		// game loop
 		while (true)
 		{
@@ -232,17 +298,26 @@ class PlayerKirk
 				goToControl = false;
 			}
 			if (goToControl) {
-				if (plan.Control == null) {
-					// keep scanning
-					Console.Error.WriteLine ("Looking for control");
+				next = Direction.None;
+				if (plan.Control != null) {
+					int len;
+					next = plan.PathFromTo (kirk, plan.Control, out len);
+					plan.PathFromTo (plan.Control, plan.Teleport, out len);
+					if (len > plan.Countdown) {
+						LogLine ("Looking for shorter path.");
+						next = Direction.None;
+					}
+				}
+				if (next == Direction.None) {
+					LogLine ("Looking for control");
 					next = plan.MaximizeDiscovery (kirk);
 				} else {
-					Console.Error.WriteLine ("Move to control");
-					next = plan.PathFromTo (kirk, plan.Control);
+					LogLine ("Move to control");
 				}
 			} else {
-				Console.Error.WriteLine ("Moving back to teleport");
-				next = plan.PathFromTo (kirk, plan.Teleport);
+				int len;
+				LogLine ("Moving back to teleport");
+				next = plan.PathFromTo (kirk, plan.Teleport, out len);
 			}
 			// Write an action using Console.WriteLine()
 			// To debug: Console.Error.WriteLine("Debug messages...");
