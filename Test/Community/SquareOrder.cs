ecbbcd6f2c29c8408e25e9f5bab9200ad1a15a91
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2016-11-29 23:05:48 +0100

    SquareOrder: finished. Spend lots of time improving my algo, failing to realize I just a silly infinite loop bug

--- Test/Community/SquareOrder.cs
+++ Test/Community/SquareOrder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 /**
@@ -20,10 +21,10 @@ Contraintes
 1 ≤ nb, label ≤ 5
 2 ≤ size ≤ 10
  **/
+
 class SolutionSquare
 {
-
-    class Square: IEnumerable<Tuple<int, int>>
+    class Square : IEnumerable<Tuple<int, int>>
     {
         public List<int> Overlays = new List<int>();
         public List<Tuple<int, int>> points = new List<Tuple<int, int>>();
@@ -34,12 +35,12 @@ class SolutionSquare
 
         public void UpdateCorners(int x, int y)
         {
-            this.points.Add(new Tuple<int, int>(x,y));
+            this.points.Add(new Tuple<int, int>(x, y));
             if (x < this.MinX)
             {
                 this.MinX = x;
             }
-            if (x>this.MaxX)
+            if (x > this.MaxX)
             {
                 this.MaxX = x;
             }
@@ -47,7 +48,7 @@ class SolutionSquare
             {
                 this.MinY = y;
             }
-            if (y>this.MaxY)
+            if (y > this.MaxY)
             {
                 this.MaxY = y;
             }
@@ -56,35 +57,68 @@ class SolutionSquare
         public List<Square> IdentifyPotentialSquares(int widht, int height)
         {
             // confirm edges
-            bool confirmLeft = false;
-            bool confirmRight = false;
-            bool confirmTop = false;
-            bool confirmBottom = false;
+            var confirmLeft = false;
+            var confirmRight = false;
+            var confirmTop = false;
+            var confirmBottom = false;
+            var countTop = 0;
+            var countBottom = 0;
+            var countLeft = 0;
+            var countRight = 0;
             // confirm edges
             foreach (var point in this.points)
             {
-                if (point.Item1 == this.MinX && point.Item2 != this.MinY && point.Item2 != this.MaxY)
+                if (point.Item1 == this.MinX)
                 {
-                    Console.Error.WriteLine("Confirmed left at {0}", this.MinX);
-                    confirmLeft = true;
+                    countLeft++;
                 }
-                if (point.Item1 == this.MaxX && point.Item2 != this.MinY && point.Item2 != this.MaxY)
+                else if (point.Item1 == this.MaxX)
                 {
-                    Console.Error.WriteLine("Confirmed right at {0}", this.MaxX);
-                    confirmRight = true;
+                    countRight++;
                 }
-                if (point.Item1 != this.MinX && point.Item2 == this.MinY && point.Item1 != this.MaxX)
+                if (point.Item2 == this.MinY)
                 {
-                    Console.Error.WriteLine("Confirmed top at {0}", this.MinY);
-                    confirmTop = true;
+                    countTop++;
                 }
-                if (point.Item1 != this.MaxX && point.Item2 == this.MaxY && point.Item1 != this.MinX)
+                else if (point.Item2 == this.MaxY)
                 {
-                    Console.Error.WriteLine("Confirmed bottom at {0}", this.MaxY);
-                    confirmBottom = true;
+                    countBottom++;
                 }
             }
-            int minDim = Math.Max(this.MaxX - this.MinX + 1, this.MaxY - this.MinY);
+            if (countLeft > 2)
+            {
+                confirmLeft = true;
+            }
+            if (countRight > 2)
+            {
+                confirmRight = true;
+            }
+            if (countTop > 2)
+            {
+                confirmTop = true;
+            }
+            if (countBottom > 2)
+            {
+                confirmBottom = true;
+            }
+            var tempHeight = this.MaxY - this.MinY + 1;
+            var tempWidth = this.MaxX - this.MinX + 1;
+            if (countLeft >= 2 && countRight >= 2 && countTop >= 2 && countBottom > 2)
+            {
+                if (tempWidth == tempHeight)
+                {
+                    confirmRight = confirmTop = confirmBottom = confirmLeft = true;
+                }
+                if (tempWidth > tempHeight)
+                {
+                    confirmRight = confirmLeft = true;
+                }
+                else
+                {
+                    confirmTop = confirmBottom = true;
+                }
+            }
+            int minDim = Math.Max(tempWidth, tempHeight);
             var result = new List<Square>();
             // if all edges are sure, no questions
             if (confirmBottom && confirmLeft && confirmTop && confirmRight)
@@ -94,19 +128,18 @@ class SolutionSquare
             else
             {
                 var minX = confirmLeft ? this.MinX : 0;
-                var maxX = confirmRight ? this.MaxX : widht-1;
+                var maxX = confirmRight ? this.MaxX : widht - 1;
                 var minY = confirmTop ? this.MinY : 0;
-                var maxY = confirmBottom ? this.MaxY : height-1;
+                var maxY = confirmBottom ? this.MaxY : height - 1;
 
                 var maxDim = Math.Min(maxX - minX + 1, maxY - minY + 1);
                 // try all possible dimensions
                 for (var size = minDim; size <= maxDim; size++)
                 {
-                    Console.Error.WriteLine("Trying size of {0}.", size);
                     // try all possible top lines
                     for (var ty = minY; ty <= this.MinY; ty++)
                     {
-                        var by = ty + size-1;
+                        var by = ty + size - 1;
                         // check if bottom line is possible
                         if (by < this.MaxY || by > maxY)
                         {
@@ -115,9 +148,9 @@ class SolutionSquare
                         // check all left column
                         for (var lx = minX; lx <= this.MinX; lx++)
                         {
-                            var rx = lx + size-1;
+                            var rx = lx + size - 1;
                             // check if right column is possible
-                            if ( rx< this.MaxX || rx > maxX)
+                            if (rx < this.MaxX || rx > maxX)
                             {
                                 continue;
                             }
@@ -143,7 +176,7 @@ class SolutionSquare
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-           return new BorderEnumerator(this);
+            return new BorderEnumerator(this);
         }
 
         private class BorderEnumerator : IEnumerator<Tuple<int, int>>
@@ -157,21 +190,9 @@ class SolutionSquare
                 Reset();
             }
 
-            public Tuple<int, int> Current
-            {
-                get
-                {
-                    return this.current;
-                }
-            }
+            public Tuple<int, int> Current => this.current;
 
-            object IEnumerator.Current
-            {
-                get
-                {
-                    return this.current;
-                }
-            }
+            object IEnumerator.Current => this.current;
 
             public void Dispose()
             {
@@ -202,6 +223,8 @@ class SolutionSquare
                     else
                     {
                         Y--;
+                        if (Y == this.pathSquare.MinY)
+                            return false;
                     }
                 }
                 else if (X == this.pathSquare.MaxX)
@@ -233,12 +256,14 @@ class SolutionSquare
         var nb = int.Parse(Console.ReadLine());
         var map = new List<string>();
         var squares = new Dictionary<int, Square>(nb);
+        var chrono = new Stopwatch();
+        chrono.Start();
         // we capture inputs and identify squares coordinates
         for (var i = 0; i < h; i++)
         {
             var line = Console.ReadLine();
             map.Add(line);
-            Console.Error.WriteLine(line);
+//            Console.Error.WriteLine(line);
             for (var l = 0; l < line.Length; l++)
             {
                 if (line[l] == '.') continue;
@@ -253,24 +278,33 @@ class SolutionSquare
         // now we identify overlapping.
         foreach (var nextSquare in squares)
         {
-            Console.Error.WriteLine("Sccaning square {0}.", nextSquare.Key);
+            Console.Error.WriteLine("Scaning square {0}({1}).", nextSquare.Key, chrono.ElapsedMilliseconds);
             var possibleSquares = nextSquare.Value.IdentifyPotentialSquares(w, h);
             Square validSquare = null;
-            Console.Error.WriteLine("Found {0} variants for square {1}", possibleSquares.Count, nextSquare.Key);
             foreach (var square in possibleSquares)
             {
-                bool errorFound = false;
-                Console.Error.WriteLine("scanning {0} = {1}:{2} - {3} {4}", nextSquare.Key, square.MinX, square.MinY,
-                    square.MaxX, square.MaxY);
+                var errorFound = false;
+//                Console.Error.WriteLine("scanning {0} = {1}:{2} - {3} {4}", nextSquare.Key, square.MinX, square.MinY,
+//                    square.MaxX, square.MaxY);
                 foreach (var coords in square)
                 {
                     var car = map[coords.Item2][coords.Item1];
+                    Console.Error.Write(car);
                     if (car == '.')
                     {
-                        Console.Error.WriteLine("Unexpected dot at {0}:{1}", coords.Item1, coords.Item2);
+ //                       Console.Error.WriteLine("Unexpected dot at {0}:{1}", coords.Item1, coords.Item2);
                         errorFound = true;
                         // rectangles was not properly identified
                         break;
+                    }
+                    var id = car - '0';
+                    if (id != nextSquare.Key)
+                    {
+                        if (!square.Overlays.Contains(id))
+                        {
+                            square.Overlays.Add(id);
+//                            Console.Error.WriteLine("Square {0} overlaps square {1}", id, nextSquare.Key);
+                        }
                     }
                 }
                 if (!errorFound)
@@ -283,27 +317,15 @@ class SolutionSquare
             nextSquare.Value.MaxX = validSquare.MaxX;
             nextSquare.Value.MinY = validSquare.MinY;
             nextSquare.Value.MaxY = validSquare.MaxY;
-            foreach (var coords in validSquare)
-            {
-                var car = map[coords.Item2][coords.Item1];
-                var id = car - '0';
-                if (id != nextSquare.Key)
-                {
-                    if (!nextSquare.Value.Overlays.Contains(id))
-                    {
-                        nextSquare.Value.Overlays.Add(id);
-                        Console.Error.WriteLine("Square {0} overlaps square {1}", id, nextSquare.Key);
-                    }
-                }
-            }
+            nextSquare.Value.Overlays = validSquare.Overlays;
         }
-        Console.Error.WriteLine("Ordering");
+        Console.Error.WriteLine("Ordering ({0})", chrono.ElapsedMilliseconds);
         var squareList = new List<int>(squares.Keys);
         var orderedSquares = new Stack<int>();
         // we still have to identify the order
         while (squareList.Count != 0)
         {
-            for (var i=0;i<squareList.Count; i++)
+            for (var i = 0; i < squareList.Count; i++)
             {
                 var overlaysFound = squares[squareList[i]].Overlays.All(overlay => orderedSquares.Contains(overlay));
                 // we can add a square to the list if all the overlays are in the list
@@ -317,7 +339,7 @@ class SolutionSquare
         foreach (var orderedSquare in orderedSquares)
         {
             var square = squares[orderedSquare];
-            Console.WriteLine("{0} {1}", orderedSquare, square.MaxX-square.MinX+1);
+            Console.WriteLine("{0} {1}", orderedSquare, square.MaxX - square.MinX + 1);
         }
     }
 }
