fb28bd7819aef73bd211e37dd97b91cd75555487
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2016-12-02 23:22:57 +0100

    TheFastest: Done

---
+++ Test/Community/TheFastest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class TheFastest
+{
+    static void MainFastest(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        string minTime = new string('Z', 10);
+        for (int i = 0; i < N; i++)
+        {
+            string t = Console.ReadLine();
+            if (t.CompareTo(minTime)<0)
+            {
+                minTime = t;
+            }
+        }
+
+        Console.WriteLine(minTime);
+    }
+}
