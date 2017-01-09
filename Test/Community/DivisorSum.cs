f4cd8cac1ddfef2d92ea8768129d0672fe04eb3f
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2017-01-09 22:47:36 +0100

    DivisorSum: DOne

---
+++ Test/Community/DivisorSum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CodingGame.Community
+{
+    class DivisorSum
+    {
+        static void MainDivisor()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var sum = 0L;
+            for (var i = 1L; i <= n; i++)
+                sum += (n / i) * i;
+            Console.WriteLine(sum);
+        }
+    }
+}
