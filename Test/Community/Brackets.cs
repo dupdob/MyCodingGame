f142b1836ea5f0cfc7cd7e02400612433265c30a
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2017-01-10 23:00:48 +0100

    Brackets: Done

---
+++ Test/Community/Brackets.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+/**
+Dire si l'expression donnée en entrée est bien parenthésée. Une expression est bien parenthésée si les parenthèses (), les crochets [] et les accolades {} sont correctement appairés.
+
+L'expression ne contient pas d'espaces.
+/*
+
+ */
+class Brackets
+{
+    private static TimeSpan start;
+    class Bracket
+    {
+        public char Marker;
+        public int Count;
+    }
+    static void Main(string[] args)
+    {
+        var expressions = Console.ReadLine();
+        var stack = new Stack<char>();
+        var good = true;
+        foreach (var car in expressions)
+        {
+            switch (car)
+            {
+                case '(':
+                case '{':
+                case '[':
+                    stack.Push(car);
+                    break;
+                case ')':
+                    if (stack.Count == 0 || stack.Pop()!='(')
+                    {
+                        good = false;
+                    }
+                    break;
+                case '}':
+                    if (stack.Count == 0 || stack.Pop()!='{')
+                    {
+                        good = false;
+                    }
+                    break;
+                case ']':
+                    if (stack.Count == 0 || stack.Pop()!='[')
+                    {
+                        good = false;
+                    }
+                    break;
+            }
+        }
+        if (stack.Count > 0)
+            good = false;
+        // text
+        Console.WriteLine(good ? "true" : "false");
+    }
+
+}
---
+++ Test/Community/EinsteinRiddle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingGame.Community
+{
+    class EinsteinRiddle
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs;
+            inputs = Console.ReadLine().Split(' ');
+            int nbCharacteristics = int.Parse(inputs[0]);
+            int nbPeople = int.Parse(inputs[1]);
+            var dimensions = new List<string>[nbCharacteristics];
+            for (int i = 0; i < nbCharacteristics; i++)
+            {
+                dimensions[i] = Console.ReadLine().Split(' ').ToList();
+            }
+            int nbLinks = int.Parse(Console.ReadLine());
+            for (int i = 0; i < nbLinks; i++)
+            {
+                string link = Console.ReadLine();
+            }
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+            Console.WriteLine("Amelia Bob Charles Daniel\nCar Autobus Bicycle Roller\nTree Flower Bush Herb\nTurtle Elephant Rhinoceros Ant");
+        }
+    }
+}
