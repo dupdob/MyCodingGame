789f9581611f10b20856b26ac7ad40ce970977fa
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2017-01-18 22:45:53 +0100

    CodingDice: Starting HitTheRoad: Starting

---
+++ Test/Community/CodinDice.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+/**
+ Rossi is a cute chibi devil who rolls giant dice, arranged on a grid, by walking across the top of them.
+
+Your goal is to help Rossi solve the puzzle with the minimum number of die rolls.
+
+The puzzle is solved when the following two conditions hold true:
+- The value of each die is 6, i.e. they all have 6 pips showing on the TOP as such: ⚅
+- All dice form a single connected group.
+
+
+==============================
+Moving around
+==============================
+
+- Rossi's position is on top of the last die he moved. If he hasn't moved yet, he's on top of the first die of the list.
+- Dice form a group when they are standing adjacent to one another. Diagonals do not count.
+- Rossi can walk freely between dice in the same group. Walking is not counted against the solution's length.
+- The map is always a 4 x 4 square map; the coordinates range from 0 to 3. Rossi is not allowed to move outside of the boundaries.
+
+
+==============================
+Rolling dice
+==============================
+
+- If there is a free grid space from Rossi's standing position on a die, he can roll the die there. Since he can walk freely among the group, he can thus roll any "free" die within the group boundary.
+- When a die is rolled, one of its bottom edges (the one between the old and new position) stays still, causing it to tumble and 'roll' in the relevant direction.
+- Therefore rolling a die can change where the number 6 face is.
+
+For example, if the die is rolled to the RIGHT and the 6 face was TOP, it will then be EAST. If the 6 face was BOTTOM, it will become WEST. If it was NORTH or SOUTH, it would twist but remain in the same relative position. (Pick a die and roll it in one direction, you'll see that two faces stay)
+
+- Rolling a die adds one to the solution's length.
+- We're only interested in the 6 faces' ending position, so you only need to track that face on each die.
+- Each time Rossi rolls a die, output the position of that die and the direction in which it was rolled to to STDOUT.
+
+
+==============================
+IRON dice
+==============================
+
+Some dice are made of IRON. They are very heavy.
+
+- Rossi cannot roll IRON dice. He cannot "jump" from an IRON die to an empty spot either.
+- Their 6 face is always on TOP.
+
+
+==============================
+Output to STDOUT
+==============================
+
+You must find the shortest solution to the puzzle. The puzzles are designed so that there is always only one "shortest" solution.
+
+- The shortest solution is based on the number of times a die was rolled.
+
+Try checking the test cases with real dice if you are confused! 
+  **/
+class CodinDice
+{
+    static void MainDice(string[] args)
+    {
+        var n = int.Parse(Console.ReadLine());
+        var dices = new List<Dice>(n);
+        for (var i = 0; i < n; i++)
+        {
+            var inputs = Console.ReadLine().Split(' ');
+            var dice = new Dice();
+            dice.ID = i;
+            dice.X = int.Parse(inputs[0]);
+            dice.Y = int.Parse(inputs[1]);
+            dice.Face = inputs[2];
+            dices.Add(dice);
+        }
+
+        // Write an action using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        Console.WriteLine("answer");
+    }
+
+    class Dice
+    {
+        public int ID;
+        public int X;
+        public int Y;
+        public string Face;
+
+    }
+}
---
+++ Test/Community/HitTheRoad.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+/**
+A driver is on a road network with n junctions and m roads with two specific positions s and t. 
+Each junction of the network can be associated with a time window [b,e] : the driver can go through that intersection only between times b and e. Every other junction is free : the driver can cross it at any time. Each road is associated with a duration: the time a car needs to drive through that road. The roads are directed : the car cannot go through a road backward.
+
+Your goal is to decide if a driver can go from s to t such that, for each junction associated with a time window [b,e], the moment r when the driver reaches that junction belongs to its window : b <= r <= e. If the junction has no time window, the driver can reach it at any time.
+
+The driver starts in s at time 0.
+The driver cannot wait. Once he reaches a junction, he must go though an outgoing road of that junction.
+The time windows are always closed intervals.
+
+Here is an example where <, >, ^ and v represent roads (and the directions).
+
+s > u > w > t
+
+v   ^   v   ^
+
+x > y > z > r
+
+
+The time window on x, u and y is [1,2].
+The time window on w, z and r is [3,5].
+The time window on t is [1,7].
+The duration on each arc is 1.
+
+
+It is possible to go from s to t with that path.
+
+s           t
+
+v           ^
+
+x > y > z > r
+
+
+The driver would be in x at time 1, y at time 2, z at time 3, r at time 4 and t at time 5. Each time fits within the specified time window.
+
+In this example with the same time windows and same durations :
+
+s > u > w > t
+
+v   ^   v   ^
+
+x > y   z > r
+
+
+You cannot go from s to t, because you must go through w in order to reach t and it is not possible to be in w between time 3 and 5 and in u between time 1 and 2:
+- either the driver goes from s to u and arrives at w at time 2;
+- or it uses the path s x y u w and arrives at u at time 3.
+Entrée
+Line 1 : Three integers n, m and ntw the number of junctions and roads in the network and the number of junctions with a time window.
+    The junctions are numbered from 0 to n-1.
+Line 2 : Two integers s and t, the origin and the destination of the driver
+ntw next lines : three integers v, b and e, a junction v, the beginning and the end of the time window of that junction.
+m next lines : three integers u, v and d, the origin, the destination and the duration of each arc.
+Sortie
+Line 1 : print true if there is a path from s to t satisfying the time windows constraints. Otherwise, print false.
+    **/
+class Solution
+{
+    class Junction
+    {
+        public int b;
+        public int e;
+
+        public Junction()
+        {
+            b = 0;
+            e = int.MaxValue;
+            links = new List<Junction>();
+        }
+
+        public IList<Junction> links;
+    }
+
+    class Road
+    {
+        public Junction v;
+        public int d;
+    }
+
+    static void Main(string[] args)
+    {
+        string[] inputs;
+        inputs = Console.ReadLine().Split(' ');
+        int n = int.Parse(inputs[0]);
+
+        var junctions = new Junction[n];
+        int m = int.Parse(inputs[1]);
+        int ntw = int.Parse(inputs[2]);
+        inputs = Console.ReadLine().Split(' ');
+        int s = int.Parse(inputs[0]);
+        int t = int.Parse(inputs[1]);
+        for (int i = 0; i < ntw; i++)
+        {
+            inputs = Console.ReadLine().Split(' ');
+            int v = int.Parse(inputs[0]);
+            junctions[v].b = int.Parse(inputs[1]);
+            junctions[v].e = int.Parse(inputs[2]);
+        }
+        for (int i = 0; i < m; i++)
+        {
+            inputs = Console.ReadLine().Split(' ');
+            int u = int.Parse(inputs[0]);
+            var j = new Road();
+            j.v = junctions[int.Parse(inputs[1])];
+            j.d = int.Parse(inputs[2]);
+            junctions[u].links.Add(j);
+        }
+        Console.WriteLine("true");
+    }
+}
