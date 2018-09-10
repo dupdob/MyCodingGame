783ffca4ddeeadbe13275b1e077228bee170e134
Cyrille DUPUYDAUBY <dupdob@users.noreply.github.com>
2018-09-10 16:10:34 +0200

    CodeChef: optimized ConditionZero and BadShuffle

--- CodeChefSept18/BadShuffle.cs
+++ CodeChefSept18/BadShuffle.cs
@@ -7,7 +7,7 @@ namespace CodeChefSept18
 {
     public class BadShuffle
     {
-        static void Main(string[] args)
+        static void MainShuffle(string[] args)
         {
             var cards = int.Parse(Console.ReadLine());
 
@@ -31,74 +31,6 @@ namespace CodeChefSept18
             Console.WriteLine(ListToString(highestHand));
             Console.WriteLine(ListToString(lowestProbaHand));
 
-            
-/*
-            IDictionary<Hand, int> score = new Dictionary<Hand, int>();
-            for ( var index = 0L;; index++)
-            {
-                var hand = new Hand(cards);
-                var counter = index;
-                for (var i = 0; i < cards; i++)
-                {
-                    var id = counter % cards;
-                    counter /= cards;
-                    hand.Swap(i, (int)id);
-                }
-
-                if (counter > 0)
-                {
-                    Console.WriteLine($"{index} draws");
-                    break;
-                }
-  
-                if (!score.ContainsKey(hand))
-                {
-                    score.Add(hand, 1);
-                }
-                else
-                {
-                    score[hand] += 1;
-                }
-            }
-
-            Hand minHand = null;
-            Hand maxHand = null;
-            int minProba = int.MaxValue;
-            int maxProba = 0;
-            foreach (var pair in score)
-            {
-                if (pair.Value > maxProba)
-                {
-                    maxHand = pair.Key;
-                    maxProba = pair.Value;
-                }
-
-                if (pair.Value <= minProba)
-                {
-                    minHand = pair.Key;
-                    minProba = pair.Value;
-                }
-            }
-            Console.WriteLine(maxHand);
-            Console.WriteLine(minHand);
-            
-            Console.WriteLine($"Highest proba hands ({maxProba})");
-            foreach (var entry in score)
-            {
-                if (entry.Value == maxProba)
-                {
-                    Console.WriteLine(entry.Key);
-                }
-            }
-            Console.WriteLine($"Lowest proba hands ({minProba})");
-            foreach (var entry in score)
-            {
-                if (entry.Value == minProba)
-                {
-                    Console.WriteLine(entry.Key);
-                }
-            }
- */
         }
 
         private static string ListToString(IEnumerable<int> cards)
@@ -111,69 +43,6 @@ namespace CodeChefSept18
             }
 
             return text.ToString();            
-        }
-
-        sealed class Hand
-        {
-            private int[] cards;
-
-            public Hand(int cardCount)
-            {
-                cards = Enumerable.Range(1, cardCount).ToArray();
-            }
-
-            public void Swap(int i, int j)
-            {
-                var temp = cards[j];
-                cards[j] = cards[i];
-                cards[i] = temp;
-            }
-
-            public override string ToString()
-            {
-                var text = new StringBuilder(cards.Length*3);
-                foreach (var card in cards)
-                {
-                    text.Append(card.ToString());
-                    text.Append(' ');
-                }
-
-                return text.ToString();
-            }
-
-            private bool Equals(Hand other)
-            {
-                if (cards.Length != other.cards.Length)
-                {
-                    return false;
-                }
-                for (int i = 0; i < cards.Length; i++)
-                {
-                    if (cards[i] != other.cards[i])
-                        return false;
-                }
-
-                return true;
-            }
-
-            public override bool Equals(object obj)
-            {
-                if (ReferenceEquals(null, obj)) return false;
-                if (ReferenceEquals(this, obj)) return true;
-                if (obj.GetType() != this.GetType()) return false;
-                return Equals((Hand) obj);
-            }
-
-            public override int GetHashCode()
-            {
-                var hash = 0;
-                foreach (var card in cards)
-                {
-                    hash = hash * 39 ^ card;
-                }
-
-                return hash;
-            }
         }
     }
 }
--- CodeChefSept18/ConditionZero.cs
+++ CodeChefSept18/ConditionZero.cs
@@ -1,5 +1,9 @@
+//#define TEST
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+
 
 namespace CodeChefSept18
 {
@@ -7,13 +11,27 @@ namespace CodeChefSept18
     {
         static void MainZero(string[] args)
         {
+            var totalPop = 0L;
+#if TEST
+            var map = BuildSample();
+            var N = map.Length;
+            var M = map[0].Length;
+            var K = new Random().Next(500, 1000);
+
+            Console.WriteLine($"{N}x{M} in {K}");
+            for (var i = 0; i < N; i++)
+            {
+                for (var j = 0; j < M; j++)
+                {
+                    totalPop += map[i][j];
+                }
+            }
+#else
             var parameters = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
             var N = parameters[0];
             var M = parameters[1];
             var K = parameters[2];
             var map = new int[N][];
-            var answer = new int[N, M];
-            var totalPop = 0L;
             for (var i = 0; i < N; i++)
             {
                 map[i] = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
@@ -22,40 +40,160 @@ namespace CodeChefSept18
                     totalPop += xPop;
                 }
             }
-            // ideal size of each
-            var targetPop = totalPop / K;
+#endif
+            // try down, then left to right strategy
+            IScanner cursor = new DownLToR(M, N);
+            var answer = new int[N, M];
+            var score = FillMap(map, answer, cursor, K, totalPop);
+            // try left to right, then down
+            cursor = new LToRDown(M, N);
+            
+            var answer2 = new int[N, M];
+            var score2 = FillMap(map, answer2, cursor, K, totalPop);
+            if (score > score2)
+            {
+                Log($"LToR is better ({score2} instead of {score})");
+                answer = answer2;
+                score = score2;
+            }
+            
+            // sanity check
+#if TEST           
+            for (var i = 0; i < N; i++)
+            {
+                for (var j = 0; j < M; j++)
+                {
+                    var cell = answer[i, j];
+                    if (cell == 0)
+                    {
+                        Console.WriteLine($"{j}:{i}: not filled");
+                    }
 
-            var cursor = new DownLToR(M, N);
+                    if (i > 0 && cell == answer[i - 1, j])
+                    {
+                        continue;
+                    }
+                    if (j > 0 && cell == answer[i , j -1])
+                    {
+                        continue;
+                    }
+                    if (i < N-1 && cell == answer[i+1 , j])
+                    {
+                        continue;
+                    }
+                    if (j < M-1 && cell == answer[i , j + 1])
+                    {
+                        continue;
+                    }
+                    Console.WriteLine($"{j}:{i}: isolated");
+                }
+            }
+            Console.WriteLine($"score: {score}, out of {totalPop} ({N}x{M}/{K} ~{totalPop/K}).");
+#else
+            for (var i = 0; i < N; i++)
+            {
+                var builder = new StringBuilder(M * 4);
+                for (var j = 0; j < M; j++)
+                {
+                    builder.Append(answer[i, j].ToString());
+                    builder.Append(' ');
+                }
+                Console.WriteLine(builder.ToString());
+            }
+#endif
+        }
+
+        private static void Log(string text)
+        {
+#if TEST
+            Console.WriteLine(text);
+#endif
+            
+        }
+
+        private static int ComputeScore(long[] zonePops)
+        {
+            var minPop = long.MaxValue;
+            var maxPop = 0L;
+
+            foreach (var pop in zonePops)
+            {
+                if (pop < minPop)
+                {
+                    minPop = pop;
+                }
+
+                if (pop > maxPop)
+                {
+                    maxPop = pop;
+                }
+            }
+
+            var score = maxPop - minPop;
+            return (int)score;
+        }
+
+        private static int FillMap(IReadOnlyList<int[]> map, int[,] answer, IScanner cursor, int k, long totalPop)
+        {
+            var zonePops = new long[k];
             var totalError = 0L;
             var zoneId = 0;
-            var zonePops = new long[K];
+            var targetPop = totalPop / k;
+            var cumulatedPopulation = 0L;
             do
             {
                 var thisZone = map[cursor.Y][cursor.X];
-                var total = zonePops[zoneId] - targetPop + totalError;
-                if (total + thisZone > 0 && Math.Abs(total + thisZone) > Math.Abs(total))
+                var total = cumulatedPopulation - targetPop;
+                if (zonePops[zoneId] > 0 && total + thisZone > 0 && Math.Abs(total + thisZone) > Math.Abs(total))
                 {
-                    if (zoneId<K-1)
+                    if (zoneId < k - 1)
                         zoneId++;
-                    totalError = total;
+                    targetPop = totalPop * (zoneId + 1) / k;
                 }
-                answer[cursor.Y, cursor.X] = zoneId+1;
 
+                answer[cursor.Y, cursor.X] = zoneId + 1;
+                cumulatedPopulation += thisZone;
                 zonePops[zoneId] += thisZone;
             } while (cursor.Next());
 
-
-            for (int i = 0; i < N; i++)
+            if (zoneId < k - 1)
             {
-                for (int j = 0; j < M; j++)
-                {
-                    Console.Write($"{answer[i,j]} ");
-                }
-                Console.WriteLine();
+                Log($"Failed: {zoneId+1} instead of {k}");
+ 
+                return int.MaxValue;
             }
+
+            return ComputeScore(zonePops);
         }
 
-        class DownLToR
+
+        private static int[][] BuildSample()
+        {
+            var rdn = new Random();
+            var N = rdn.Next(500, 1000);
+            var M = rdn.Next(500, 1000);
+            var result = new int[N][];
+            for (var i = 0; i < N; i++)
+            {
+                result[i] = new int[M];
+                for (int j = 0; j < M; j++)
+                {
+                    result[i][j] = rdn.Next(10000000);
+                }
+            }
+
+            return result;
+        }
+        
+        private interface IScanner
+        {
+            int X { get; }
+            int Y { get; }
+            bool Next();
+        }
+
+        
+        class DownLToR : IScanner
         {
             public int X { get; private set; }
             public int Y{ get; private set; }
@@ -97,5 +235,46 @@ namespace CodeChefSept18
             }
         }
 
+        class LToRDown : IScanner
+        {
+            public int X { get; private set; }
+            public int Y{ get; private set; }
+
+            private bool right = true;
+            private readonly int width;
+            private readonly int height;
+
+            public LToRDown(int width, int height)
+            {
+                this.width = width;
+                this.height = height;
+            }
+
+            public bool Next()
+            {
+                if (right)
+                {
+                    if (X == width -1)
+                    {
+                        Y++;
+                        right = false;
+                        return Y < height;
+                    }
+
+                    X++;
+                    return true;
+                }
+
+                if (X == 0)
+                {
+                    Y++;
+                    right = true;
+                    return Y < height;
+                }
+
+                X--;
+                return true;
+            }
+        }
     }
 }
