67dcee0674d6fc16173ef3c6695a5ea6cde5d91c
Cyrille DUPUYDAUBY <dupdob@users.noreply.github.com>
2019-12-02 23:14:07 +0100

    2015 Day 2 and Day 3

---
+++ AdventCalendar2015/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AdventCalendar2015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var day = new Day3();
+
+            day.ParseInput();
+            Console.WriteLine("Answer 1: {0}", day.VisitedHouses());
+            Console.WriteLine("Answer 2: {0}", day.VisitedHousesWithRobot());
+        }
+    }
+}
