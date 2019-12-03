072ddd7f58884c00d6701bb863bf21c62fed125a
Cyrille DUPUYDAUBY <dupdob@users.noreply.github.com>
2019-12-03 16:07:51 +0100

    Add day 3 for AdventOfCode2019

--- AdventCalendar2019/Program.cs
+++ AdventCalendar2019/Program.cs
@@ -6,31 +6,14 @@ namespace AdventCalendar2019
     {
         static void Main(string[] args)
         {
-            var puzzle = new Day2();
+            var puzzle = new Day3();
 
-            var original = puzzle.ParseInput(Day2.Input);
-            var input =  (int[]) original.Clone();
-            input[1] = 12;
-            input[2] = 2;
-         
+            puzzle.Parse();
+            
             Console.Write("Answer 1: ");
-            Console.WriteLine(puzzle.ComputeAnswer(input));
-
-            for (var i = 0; i < 99; i++)
-            {
-                for (int j = 0; j < 99; j++)
-                {
-                    input =  (int[]) original.Clone();
-                    input[1] = i;
-                    input[2] = j;
-                    var result = puzzle.ComputeAnswer(input);
-                    if (result == 19690720)
-                    {
-                        Console.WriteLine("Answer 2: {0}", i*100+j);
-                        break;
-                    }
-                }
-            }
+            Console.WriteLine(puzzle.FindCloserIntersection());
+            Console.Write("Answer 2: ");
+            Console.WriteLine(puzzle.FindShortestPathToIntersection());
         }
     }
 }
