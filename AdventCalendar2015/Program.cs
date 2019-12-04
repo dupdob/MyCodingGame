57d4ba3e8736976bad77985970f7e358ba9834a1
Cyrille DUPUYDAUBY <dupdob@users.noreply.github.com>
2019-12-04 07:06:16 +0100

    Add 2015 Day 8

--- AdventCalendar2015/Program.cs
+++ AdventCalendar2015/Program.cs
@@ -6,9 +6,9 @@ namespace AdventCalendar2015
     {
         static void Main(string[] args)
         {
-            var day = new Day7();
-            Console.WriteLine("Answer 1: {0}", day.FindSignal_a());
-            Console.WriteLine("Answer 2: {0}", day.FindSignal_aWhenForcing());
+            var day = new Day8();
+            Console.WriteLine("Answer 1: {0}", day.SumOverhead());
+            Console.WriteLine("Answer 2: {0}", day.SumEncodedOverhead());
         }
     }
 }
