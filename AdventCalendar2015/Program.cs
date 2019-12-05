a019584e56cf83f0372d5463c1359d91ad67341a
Cyrille DUPUYDAUBY <dupdob@users.noreply.github.com>
2019-12-05 08:18:53 +0100

    Add Day 5 2019

--- AdventCalendar2015/Program.cs
+++ AdventCalendar2015/Program.cs
@@ -2,13 +2,14 @@
 
 namespace AdventCalendar2015
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            var day = new Day8();
-            Console.WriteLine("Answer 1: {0}", day.SumOverhead());
-            Console.WriteLine("Answer 2: {0}", day.SumEncodedOverhead());
+            var day = new Day9();
+            day.Parse();
+            Console.WriteLine("Answer 1: {0}", day.FindShortestPath());
+            Console.WriteLine("Answer 2: {0}", day.FindLongestPath());
         }
     }
 }
